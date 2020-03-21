70fb06fd560d5fcecd34767cba47c243056dea33
Wiktor Jagusz <wiktor.jagusz@icloud.com>
2020-03-21 20:56:35 +0100

    Navigation

--- js/Navigation.js
+++ js/Navigation.js
@@ -4,9 +4,10 @@ import React, { Component } from "react";
 
 export default class Navigation extends Component {
     render() {
-        const menuClass = this.props.showBlock === "none" ? "menu-open" : "menu-closed"
-        if (this.props.showBlock === "none") {
-            return (<>
+        const menuClass = this.props.showBlock === "none"? "menu-open" : "menu-closed"
+        
+        return (
+            <>
                 <ul className={`nav justify-content-center ${menuClass}`} >
                     <li className="nav-item">
                         <a className="nav-link" href="#" onClick={() => this.props.changeState("jumbotron")}>Strona główna</a>
@@ -24,44 +25,15 @@ export default class Navigation extends Component {
                         <a className="nav-link" href="#" onClick={() => this.props.changeState("api")}>Api</a>
                     </li>
                 </ul>
-            </>)
-        } else {
-            return (<>
+
+
                 <div className={"closeX"}>
                     <button type="button" className="close" aria-label="Close" onClick={() => this.props.changeState('none')}>
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-            </>)
-        }
-        // return (
-        //     <>
-        //         <ul className={`nav justify-content-center ${menuClass}`} >
-        //             <li className="nav-item">
-        //                 <a className="nav-link" href="#" onClick={() => this.props.changeState("jumbotron")}>Strona główna</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a className="nav-link" href="#" onClick={() => this.props.changeState("about")}>O mnie</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a className="nav-link" href="#" onClick={() => this.props.changeState("gallery")}>Galeria</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a className="nav-link" href="#" onClick={() => this.props.changeState("contact")}>Kontakt</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a className="nav-link" href="#" onClick={() => this.props.changeState("api")}>Api</a>
-        //             </li>
-        //         </ul>
-
-
-        //         <div className={"closeX"}>
-        //             <button type="button" className="close" aria-label="Close" onClick={() => this.props.changeState('none')}>
-        //                 <span aria-hidden="true">&times;</span>
-        //             </button>
-        //         </div>
-        //     </>
-        // )
+            </>
+        )
 
     }
 }
